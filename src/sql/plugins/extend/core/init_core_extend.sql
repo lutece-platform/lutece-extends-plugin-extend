347ac809d9d89c46525f69fc254e50c6688d249b
evdngsl <158455076+evdngsl@users.noreply.github.com>
2026-01-28 09:44:37 +0100

    LUT-31759 : Fix liquibase tags for MySQL

--- src/sql/plugins/extend/core/init_core_extend.sql
+++ src/sql/plugins/extend/core/init_core_extend.sql
@@ -1,6 +1,6 @@
---liquibase formatted sql
---changeset extend:init_core_extend.sql
---preconditions onFail:MARK_RAN onError:WARN
+-- liquibase formatted sql
+-- changeset extend:init_core_extend.sql
+-- preconditions onFail:MARK_RAN onError:WARN
 --
 -- Init  table core_admin_right
 --
--- src/sql/plugins/extend/upgrade/update_db_core_extend_1.0.0-1.0.1.sql
+++ src/sql/plugins/extend/upgrade/update_db_core_extend_1.0.0-1.0.1.sql
@@ -1,6 +1,6 @@
---liquibase formatted sql
---changeset extend:update_db_core_extend_1.0.0-1.0.1.sql
---preconditions onFail:MARK_RAN onError:WARN
+-- liquibase formatted sql
+-- changeset extend:update_db_core_extend_1.0.0-1.0.1.sql
+-- preconditions onFail:MARK_RAN onError:WARN
 UPDATE core_admin_right SET id_right = 'RESOURCE_EXTENDER_MANAGEMENT', name = 'extend.adminFeature.resource_extenders_management.name', description = 'extend.adminFeature.resource_extenders_management.description' WHERE id_right = 'RESOURCE_EXTENDER_MANAGEMENT_BY_RESOURCE';
 UPDATE core_user_right SET id_right = 'RESOURCE_EXTENDER_MANAGEMENT' WHERE id_right = 'RESOURCE_EXTENDER_MANAGEMENT_BY_RESOURCE';
 DELETE FROM core_user_right WHERE id_right = 'RESOURCE_EXTENDER_MANAGEMENT_BY_RESOURCE_TYPE';
